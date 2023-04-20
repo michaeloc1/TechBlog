c28dac5f4661d59b88cf4e7302a89358f9276dfc
michaeloc1 <michaeloc@verizon.net>
2023-04-20 10:33:22 -0400

    changed redirect when blog post created

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -14,7 +14,7 @@ const newBlogpostHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to create blogpost');
     }
